3dfaedf77f3d983f76c694b2c11be36213477b19
Bill <liboning2011@163.com>
2024-01-21 14:48:22 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,22 +6,7 @@ import { ref } from 'vue'
   <div class="about-wrapper">
     <div class="title-1">关于</div>
     <div class="content">
-      一个公益图床服务。<br /><br />
-     由bill搭建<br /><br />
-      <ul>
-        <li>支持点击、拖拽、复制图片上传</li>
-        <li>
-          谷歌 <a href="https://squoosh.app" target="_blank">squoosh</a>
-          压缩图片
-        </li>
-        <li>
-          <a href="https://fancyapps.com" target="_blank">Fancybox</a>
-          的加持，让图片的预览效果更好。
-        </li>
-        <li>安全且快速，数据只存在本地。</li>
-        <li>复制转换 Markdown 格式</li>
-      </ul>
-      <br />
+      一个公益图床服务。由bill搭建。
     </div>
 </template>
 
